21309080982e799bb12138d3f4ec96c6cc86a65b
nanchuci <71806013+nanchuci@users.noreply.github.com>
2021-02-08 19:00:04 +0800

    add e8820s

--- release/src/router/rc/init.c
+++ release/src/router/rc/init.c
@@ -3811,6 +3811,56 @@ int init_nvram(void)
 		break;
 #endif /*  RTAC85U  */
 
+#if defined(RTE8820S)
+	case MODEL_RTE8820S:
+		nvram_set("boardflags", "0x100"); // although it is not used in ralink driver, set for vlan
+		nvram_set("vlan1hwname", "et0");  // vlan. used to get "%smacaddr" for compare and find parent interface.
+		nvram_set("vlan2hwname", "et0");  // vlan. used to get "%smacaddr" for compare and find parent interface.
+		nvram_set("lan_ifname", "br0");
+		wl_ifaces[WL_2G_BAND] = "ra0";
+		wl_ifaces[WL_5G_BAND] = "rai0";
+		set_basic_ifname_vars("eth3", "vlan1", wl_ifaces, "usb", "vlan1", NULL, "vlan3", NULL, 0);
+
+		nvram_set_int("btn_rst_gpio",  18|GPIO_ACTIVE_LOW);
+		nvram_set_int("btn_wps_gpio",  8|GPIO_ACTIVE_LOW);
+		nvram_set_int("btn_rfkill_gpio",10|GPIO_ACTIVE_LOW);
+		nvram_set_int("btn_wltog_gpio",10|GPIO_ACTIVE_LOW);
+		nvram_set_int("led_sys_gpio", 16|GPIO_ACTIVE_LOW);
+		nvram_set_int("led_pwr_gpio",  3|GPIO_ACTIVE_LOW);
+		nvram_set_int("led_wps_gpio",  7|GPIO_ACTIVE_LOW);
+		nvram_set_int("led_5g_gpio", 12|GPIO_ACTIVE_LOW);
+		nvram_set_int("led_2g_gpio", 14|GPIO_ACTIVE_LOW);
+//		nvram_set_int("led_all_gpio", 10|GPIO_ACTIVE_LOW);
+
+		eval("rtkswitch", "11");
+
+		/* enable bled */
+		config_swports_bled("led_wan_gpio", 0);
+		config_swports_bled("led_lan_gpio", 0);
+		config_netdev_bled("led_2g_gpio", "ra0");
+		config_netdev_bled("led_5g_gpio", "rai0");
+		config_usbbus_bled("led_usb_gpio", "1 2");
+
+		nvram_set("ehci_ports", "1-1");
+		nvram_set("ohci_ports", "2-1");
+		nvram_set("ct_max", "300000"); // force
+
+		if (nvram_get("wl_mssid") && nvram_match("wl_mssid", "1"))
+			add_rc_support("mssid");
+		add_rc_support("2.4G 5G update usbX1");
+		add_rc_support("rawifi");
+		add_rc_support("switchctrl");
+		add_rc_support("manual_stb");
+		add_rc_support("11AC");
+		//add_rc_support("pwrctrl");
+		// the following values is model dep. so move it from default.c to here
+		nvram_set("wl0_HT_TxStream", "4");
+		nvram_set("wl0_HT_RxStream", "4");
+		nvram_set("wl1_HT_TxStream", "4");
+		nvram_set("wl1_HT_RxStream", "4");
+		break;
+#endif /* RT-E8820S */
+
 #if defined(RTMIR3G) || defined(RTMIR3P) || defined(RTMIR4A) || defined(RTRM2100) || defined(RTR2100)
 #if defined(RTMIR3G)
 	case MODEL_RTMIR3G:
@@ -9074,7 +9124,7 @@ int init_nvram(void)
 	}
 
 	// wrs - white and black list
-#if !defined(RTMIR3G) && !defined(RTMIR3P) && !defined(RTMIR4A) && !defined(RTRM2100) && !defined(RTR2100)
+#if !defined(RTMIR3G) && !defined(RTE8820S) && !defined(RTMIR3P) && !defined(RTMIR4A) && !defined(RTRM2100) && !defined(RTR2100)
 	add_rc_support("wrs_wbl");
 #endif
 #endif
@@ -11343,7 +11393,7 @@ int reboothalt_main(int argc, char *argv[])
 	_dprintf(reboot ? "Rebooting..." : "Shutting down...");
 	kill(1, reboot ? SIGTERM : SIGQUIT);
 
-#if defined(RTN14U) || defined(RTN65U) || defined(RTAC52U) || defined(RTAC51U) || defined(RTN11P) || defined(RTN300) || defined(RTN54U) || defined(RTCONFIG_QCA) || defined(RTAC1200HP) || defined(RTN56UB1) || defined(RTAC54U) || defined(RTN56UB2) || defined(RTAC85U) || defined(RTAC85P) || defined(RTN800HP) || defined(RTACRH26) || defined(RTMIR3G) || defined(RTMIR3P) || defined(RTMIR4A) || defined(RTRM2100) || defined(RTR2100)
+#if defined(RTN14U) || defined(RTN65U) || defined(RTAC52U) || defined(RTAC51U) || defined(RTN11P) || defined(RTN300) || defined(RTN54U) || defined(RTCONFIG_QCA) || defined(RTAC1200HP) || defined(RTN56UB1) || defined(RTAC54U) || defined(RTN56UB2) || defined(RTAC85U) || defined(RTAC85P) || defined(RTN800HP) || defined(RTACRH26) || defined(RTMIR3G) || defined(RTE8820S) || defined(RTMIR3P) || defined(RTMIR4A) || defined(RTRM2100) || defined(RTR2100)
 	def_reset_wait = 50;
 #endif
 
